f6c6b3d6bcb0934d1a5ff57bcec1783891155162
Hanno Schlichting <hanno@jarn.com>
2011-02-16 19:12:53 +0100

    First version of the catalog optimize script

---
+++ catalogoptimize.py
@@ -0,0 +1,91 @@
+import transaction
+from Acquisition import aq_base
+from BTrees.IOBTree import IOBTree
+from BTrees.OOBTree import OOBTree
+from Products.ZCatalog.ZCatalog import ZCatalog
+from Products.ZCTextIndex.Lexicon import Lexicon
+from Products.ZCTextIndex.ZCTextIndex import ZCTextIndex
+
+
+def blen(bucket):
+    buckets = 0
+    while True:
+        buckets += 1
+        bucket = bucket._next
+        if bucket is None:
+            break
+    return buckets
+
+
+def optimize_tree(parent, k, v, attr=True):
+    bucket = getattr(v, '_firstbucket', None)
+    if bucket is None:
+        return 0
+    result = 0
+    before = blen(bucket)
+    klass = v.__class__
+    new = klass(v)
+    after = blen(new._firstbucket)
+    if after < before:
+        if attr:
+            setattr(parent, k, new)
+        else:
+            parent[k] = new
+        parent._p_changed = True
+        result += before - after
+        transaction.commit()
+    else:
+        conn = parent._p_jar
+        if conn:
+            conn.cacheGC()
+    return result
+
+
+def optimize(obj, no_data=False):
+    obj = aq_base(obj)
+    result = 0
+    obj._p_activate()
+    for k, v in obj.__dict__.items():
+        if no_data and k == 'data':
+            # data blows up memory too much
+            continue
+        result += optimize_tree(obj, k, v)
+        # handle sets inside *OBTrees
+        if isinstance(v, (IOBTree, OOBTree)):
+            new_v = obj.__dict__[k]
+            for k2, v2 in new_v.iteritems():
+                result += optimize_tree(new_v, k2, v2, attr=False)
+    print('Optimized away %s buckets in %s' % (result, obj))
+    return result
+
+
+# Loop over all Plone sites
+for site in app.values():
+    if not site.meta_type == 'Plone Site':
+        continue
+
+    site_id = site.getId()
+    print('Starting catalog optimization for site "%s" ...' % site_id)
+    combined = 0
+    for zcatalog in site.values():
+        if not isinstance(zcatalog, ZCatalog):
+            continue
+        zcatalog_id = zcatalog.getId()
+        print('Optimizing "%s"' % zcatalog_id)
+        catalog = zcatalog._catalog
+        # optimize paths, uids, data - skip data for portal_catalog
+        combined += optimize(catalog, no_data=zcatalog_id == 'portal_catalog')
+        # optimize lexica
+        for obj in zcatalog.values():
+            if isinstance(obj, Lexicon):
+                combined += optimize(obj)
+        # optimize indexes
+        for index in catalog.indexes.values():
+            if isinstance(index, ZCTextIndex):
+                combined += optimize(index.index)
+            else:
+                combined += optimize(index)
+    print('Optimized away %s buckets for site "%s"' % (combined, site_id))
+
+print('Finishing...')
+transaction.commit()
