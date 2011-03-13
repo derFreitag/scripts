14cb6af436f7f3b973d0a55eb9432f80ba180123
Helge Tesdal <tesdal@jarn.com>
2011-03-13 20:39:22 +0100

    Optimize fill rate by inserting synthetic values to split last buckets

--- catalogoptimize.py
+++ catalogoptimize.py
@@ -32,6 +32,32 @@ def blen(bucket, track_objects=False):
     return (distribution, objects)
 
 
+def get_max_bucket_size(data):
+    # Data is tree or treeset. 
+    # We calculate instead of hardcoding because values can be patched.
+    tmp = data.__class__()
+    if hasattr(tmp, 'items'):
+        update = lambda x:(x,x)
+    else:
+        update = lambda x:x
+    count = 0
+    tmp.update([update(count)])
+    bucket = tmp._firstbucket
+    while bucket._next is None:
+        count += 1
+        tmp.update([update(count)])
+    # Buckets are split on count
+    return count
+
+
+def get_bucket_sizes(bucket):
+    sizes = []
+    while bucket is not None:
+        sizes.append(len(bucket))
+        bucket = bucket._next
+    return sizes
+
+
 def optimize_tree(parent, k, v, attr=True):
     transaction.begin()
     bucket = getattr(v, '_firstbucket', None)
@@ -46,17 +72,38 @@ def optimize_tree(parent, k, v, attr=True):
     before_distribution, objects = blen(bucket, track_objects=track_objects)
     before = sum(before_distribution.values())
     klass = v.__class__
+    
+    # Gather stats like max bucket size, average bucket size and median bucket size
+    maxsize = get_max_bucket_size(v)
+    averagesize = sum([kk*vv for kk,vv in before_distribution.items()])*1.0/before
+    bucketsizes = [x for sublist in [(kk,)*vv for kk,vv in sorted(before_distribution.items())] for x in sublist]
+    median = bucketsizes[before/2]
 
-    # Fill the tree in a two-step process, which should result in better
-    # fill rates
+    # Filling the tree in a two-step process. The first time we set up the tree,
+    # values are inserted sequentially, resulting in 50% fill rate.
+    # The second time we fill up with additional values to get fill rate higher
+    # than 50%.
+    # We want to set optimal fill rates based on current fill rate.
+    # Fill rates of 55% or below indicates sequential index like dateindex
+    # and we want 100% fill rate, otherwise 90% is good.
+    avgrate = float(averagesize)/maxsize
+    medianrate = float(median)/maxsize
+    if avgrate < 0.55 or medianrate < 0.55 or medianrate > 0.95:
+        modfactor = 2 # same number of items in both runs gives 100% fill
+    else:
+        modfactor = 9 # 5 in first run and 4 in second run gives 90% fill rate
     new = klass()
     count = 0
     tmp = []
+    # If the last bucket is not 50% full after first run (it is fuller), it is likely
+    # to split on second run, and the last 3 buckets will have lower fill rates,
+    # instead of just the last one.
+    # Idea: keep the tmp the same size as max and start on 2nd run inbetween 1st run
+    # but with a max size delay
     if hasattr(v, 'items'):
         # BTree
         for kk,vv in v.items():
-            # We aim for 90% fill rate to avoid too many immediate splits
-            modcount = count % 9
+            modcount = count % modfactor
             if modcount % 2 == 0:
                 new[kk] = vv
             else:
@@ -65,11 +112,34 @@ def optimize_tree(parent, k, v, attr=True):
     else:
         # Tree set
         for kk in v.keys():
-            if count % 2 == 0:
+            modcount = count % modfactor
+            if modcount % 2 == 0:
                 new.insert(kk)
             else:
                 tmp.append(kk)
             count += 1
+    # Before adding the rest of the data, we need to make sure the last bucket
+    # is not more than 50% full.
+    # Add and remove synthetic values to provoke a bucket split
+    maxkey = new.maxKey()
+    if isinstance(maxkey, int):
+        synthetic = range(new.maxKey()+1, new.maxKey()+2+(maxsize-get_bucket_sizes(new._firstbucket)[-1]))
+    elif isinstance(maxkey, basestring):
+        synthetic = [maxkey+str(x) for x in range((maxsize-get_bucket_sizes(new._firstbucket)[-1])+1)]
+    else:
+        synthetic = []
+            
+    if hasattr(v, 'items'):
+        for s in synthetic:
+            new[s] = 0
+        for s in synthetic:
+            del new[s]
+    else:
+        for s in synthetic:
+            new.insert(s)
+        for s in synthetic:
+            new.remove(s)
+
     # Add the rest of the data
     new.update(tmp)
 
@@ -95,8 +165,10 @@ def optimize_tree(parent, k, v, attr=True):
                 many_buckets[k] = v
             else:
                 few_buckets.append(k)
+        afteravg = sum([k*v for k,v in after_distribution.items()])*1.0/after
         print('New buckets {fill size: count}: %s\nSingle buckets: %s' % (
             str(many_buckets), str(few_buckets)))
+        print('Avg before %.4f after %.4f\n' % (avgrate,float(afteravg)/maxsize))
         transaction.commit()
     else:
         conn = parent._p_jar
